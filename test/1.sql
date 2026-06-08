8a8c433d895046e1f702f7a46a61f72e0e397c42
Chaitanya <104759918+Chai5K5@users.noreply.github.com>
2026-06-08 14:54:29 +0530

    Update 1.sql

--- test/1.sql
+++ test/1.sql
@@ -1,1 +1,10 @@
 
+SELECT 
+  EXTRACT("MONTH" FROM ua.event_date) as month,  
+  COUNT(DISTINCT ua.user_id) as monthly_active_users
+FROM user_actions ua
+JOIN user_actions ua1
+ON ua1.user_id = ua.user_id
+WHERE EXTRACT("MONTH" FROM ua.event_date) = 7
+AND EXTRACT("MONTH" FROM ua1.event_date) = 6
+GROUP BY (EXTRACT("MONTH" FROM ua.event_date));
